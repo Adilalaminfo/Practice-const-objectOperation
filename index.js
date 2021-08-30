c8ff09db6ff27d6fe3a73f589acb799a29486e62
Md Adil Alam <31787784+Adilalaminfo@users.noreply.github.com>
2021-08-30 17:42:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// let x="Md Adil Alam";
+// let times=4;
+
+// if(times>3){
+
+//     let x1="I am fine";
+//     console.log(x1);
+// }
+// console.log(x1);
+// let x=5;
+// let a=9;
+
+// if(a>3){
+//     let x1="98765";
+//     console.log(x1);
+// }
+// console.log(x);
+
+const greeting={
+    message:"Adil",
+    time:3
+}
+greeting.message="Md Adil Alam";
+console.log(greeting.message);
+console.log(greeting);
+
